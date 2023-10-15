592335469ab3c6ad65b1c8e2ffa3e9e1f0e6826e
Kakapo <mastertroll123fig@gmail.com>
2023-10-16 10:13:55 +1300

    changing aboutme css

--- client/src/components/aboutMe.js
+++ client/src/components/aboutMe.js
@@ -12,7 +12,7 @@ const AboutMe = () => {
 
   return (
     <div className="section section1" id="about">
-      <div className="aboutMe">
+      <div className="rightbox aboutMe">
         <div
           className={`bubble ${isHovered ? "hovered" : ""}`}
           onMouseEnter={handleHover}
