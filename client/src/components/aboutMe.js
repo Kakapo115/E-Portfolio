61fe60aa91506de114c0b5e3be91ed53c206166c
Kakapo <mastertroll123fig@gmail.com>
2023-10-16 15:51:08 +1300

    changing aboutme

--- client/src/components/aboutMe.js
+++ client/src/components/aboutMe.js
@@ -22,7 +22,7 @@ const AboutMe = () => {
 
   return (
     <div className="section section1" id="about">
-      <div className="aboutMe backgroundcolooor">
+      <div className="aboutMe">
         <div
           className={`bubble ${isHovered ? "hovered" : ""}`}
           onMouseEnter={handleHover}
@@ -88,7 +88,18 @@ const AboutMe = () => {
           </div>
         </div>
       </div>
-      <SocialIcons />
+      <div className="icons">
+        <a href="https://github.com/Kakapo115">
+          <img src="GitHub-Logo.png" className="icon git" alt="Halfway Climb" />
+        </a>
+        <a href="https://www.linkedin.com/in/ricky-syme-b79701232/">
+          <img
+            src="linkdin-icon.png"
+            className="icon linkdin"
+            alt="Halfway Climb"
+          />
+        </a>
+      </div>
       <img src="midjour_basecamp_topcut.png" className="img" alt="Base Camp" />
     </div>
   );
