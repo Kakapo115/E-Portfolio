787019297909abffd6907335cbf729ea14cf01f1
Kakapo <mastertroll123fig@gmail.com>
2023-10-18 13:56:41 +1300

    changing bubble css for icons spinning

--- client/src/components/aboutMe.js
+++ client/src/components/aboutMe.js
@@ -74,20 +74,24 @@ const AboutMe = () => {
             )}
           </div>
         </div>
-        <a href="https://github.com/Kakapo115">
-          <img
-            src="GitHub-Logo.png"
-            className="icon git bubble"
-            alt="Halfway Climb"
-          />
-        </a>
-        <a href="https://www.linkedin.com/in/ricky-syme-b79701232/">
-          <img
-            src="linkdin-icon.png"
-            className="icon linkdin bubble"
-            alt="Halfway Climb"
-          />
-        </a>
+        <div className="bubble">
+          <a href="https://github.com/Kakapo115">
+            <img
+              src="GitHub-Logo.png"
+              className="icon git"
+              alt="Halfway Climb"
+            />
+          </a>
+        </div>
+        <div className="bubble">
+          <a href="https://www.linkedin.com/in/ricky-syme-b79701232/">
+            <img
+              src="linkdin-icon.png"
+              className="icon linkdin"
+              alt="Halfway Climb"
+            />
+          </a>
+        </div>
       </div>
       <img src="midjour_basecamp_topcut.png" className="img" alt="Base Camp" />
     </div>
