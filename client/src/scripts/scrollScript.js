fcd8f44568fc2abed964dee257c81ae0452ea89a
Kakapo <mastertroll123fig@gmail.com>
2023-10-18 16:45:03 +1300

    adding back in scroll script

--- client/src/scripts/scrollScript.js
+++ client/src/scripts/scrollScript.js
@@ -1,32 +1,32 @@
-// export function setupScroll() {
-//   const links = document.querySelectorAll(".header-right a");
+export function setupScroll() {
+  const links = document.querySelectorAll(".header-right a");
 
-//   links.forEach(function (link) {
-//     link.addEventListener("click", function (event) {
-//       event.preventDefault();
-//       const targetSection = document.querySelector(link.getAttribute("href"));
-//       const targetOffset =
-//         targetSection.getBoundingClientRect().top + window.pageYOffset;
+  links.forEach(function (link) {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+      const targetSection = document.querySelector(link.getAttribute("href"));
+      const targetOffset =
+        targetSection.getBoundingClientRect().top + window.pageYOffset;
 
-//       window.scroll({
-//         top: targetOffset,
-//         behavior: "smooth",
-//       });
-//     });
-//   });
+      window.scroll({
+        top: targetOffset,
+        behavior: "smooth",
+      });
+    });
+  });
 
-//   document.addEventListener("scroll", function () {
-//     // Check if the screen size is desktop-sized
-//     if (window.innerWidth >= 1024) {
-//       const sections = document.querySelectorAll(".section");
-//       const opacityFactor = 0.9; // Adjust this factor to control the rate of opacity change
+  document.addEventListener("scroll", function () {
+    // Check if the screen size is desktop-sized
+    if (window.innerWidth >= 1024) {
+      const sections = document.querySelectorAll(".section");
+      const opacityFactor = 0.9; // Adjust this factor to control the rate of opacity change
 
-//       sections.forEach(function (section) {
-//         const rect = section.getBoundingClientRect();
-//         const opacity = 1 - Math.abs(rect.top) / (window.innerHeight * opacityFactor);
+      sections.forEach(function (section) {
+        const rect = section.getBoundingClientRect();
+        const opacity = 1 - Math.abs(rect.top) / (window.innerHeight * opacityFactor);
 
-//         section.style.opacity = opacity >= 0 ? opacity : 0;
-//       });
-//     }
-//   });
-// }
+        section.style.opacity = opacity >= 0 ? opacity : 0;
+      });
+    }
+  });
+}
