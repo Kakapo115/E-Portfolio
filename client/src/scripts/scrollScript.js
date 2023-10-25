86db6b726de20c6fc3d8650657e6cbcb2c473de1
Kakapo <mastertroll123fig@gmail.com>
2023-10-25 22:06:43 +1300

    adding resizing changes for sections

--- client/src/scripts/scrollScript.js
+++ client/src/scripts/scrollScript.js
@@ -1,5 +1,3 @@
-import anime from 'animejs/lib/anime.es.js'; // Import Anime.js
-
 export function setupScroll() {
   const links = document.querySelectorAll(".header-right a");
 
@@ -10,44 +8,35 @@ export function setupScroll() {
       const targetOffset =
         targetSection.getBoundingClientRect().top + window.pageYOffset;
 
-      // Use Anime.js to animate the scroll position
-      anime({
-        targets: window,
-        scrollTop: targetOffset,
-        duration: 800, // Set the duration of the scroll animation
-        easing: 'easeInOutQuad', // Set the easing function
+      window.scroll({
+        top: targetOffset,
+        behavior: "smooth",
       });
     });
   });
 
   document.addEventListener("scroll", function () {
-    const sections = document.querySelectorAll(".section");
-    
-    sections.forEach(function (section) {
-      const rect = section.getBoundingClientRect();
-      const scale = Math.min(1, Math.max(0.5, 1 - Math.abs(rect.top) / window.innerHeight));
+    // Check if the screen size is desktop-sized
+    if (window.innerWidth >= 1024) {
+      const sections = document.querySelectorAll(".section");
+      const opacityFactor = 0.9; // Adjust this factor to control the rate of opacity change
+      const minOpacity = 0.3; // Adjust the minimum opacity as needed
 
-      // Use Anime.js to smoothly animate the scale property
-      anime({
-        targets: section.querySelector(".img"),
-        scale: scale,
-        duration: 400, // Adjust the duration of the scale animation
-        easing: 'easeInOutQuad', // Adjust the easing function
-      });
-
-      // Check if the screen size is desktop-sized for opacity animation
-      if (window.innerWidth >= 1024) {
-        const opacityFactor = 0.9;
-        const minOpacity = 0.3;
+      sections.forEach(function (section) {
+        const rect = section.getBoundingClientRect();
         const opacity =
           minOpacity +
           (1 - minOpacity) * (1 - Math.abs(rect.top) / (window.innerHeight * opacityFactor));
 
         section.style.opacity = opacity >= minOpacity ? opacity : minOpacity;
-      } else {
-        // If the screen size is smaller, set all sections to full opacity
+      });
+    } else {
+      // If the screen size is smaller, set all sections to full opacity
+      const sections = document.querySelectorAll(".section");
+      sections.forEach(function (section) {
         section.style.opacity = 1;
-      }
-    });
+      });
+    }
   });
 }
+
