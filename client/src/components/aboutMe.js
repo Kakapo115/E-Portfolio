222b67d23e080f3ce3aeca27a2ba7748b84ebf49
Kakapo <mastertroll123fig@gmail.com>
2024-07-08 12:50:52 +1200

    updating education info

--- client/src/components/aboutMe.js
+++ client/src/components/aboutMe.js
@@ -61,7 +61,7 @@ const AboutMe = () => {
     {
       title: "Education",
       content:
-        "In my pursuit of knowledge, I'm about to graduate with a Bachelor of Applied Information in mid November. My bachelor gives me a wide range of knowledge in the various areas such as .NET MAUI development, MERN Web development, game development through Unity and Unreal using C# and a little C++, and some other experiences. These combine to give me a wide variety of skills to break out into the industry with.",
+        "In my pursuit of knowledge, I graduated with a Bachelor of Applied Information in mid November 2023. My bachelor gave me a wide range of knowledge in the various areas such as .NET MAUI development, MERN Web development, game development through Unity and Unreal using C# and a little C++, and some other experiences. These combine to give me a wide variety of skills to break out into the industry with.",
     },
   ];
 
